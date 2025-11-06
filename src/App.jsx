f27db445d5cd86e9af642aa691a359c76b075d4d
Zakawatali <zakawatali6@gmail.com>
2025-11-06 20:32:35 +0500

    one more commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<About/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/*' element={<NotFound/>}/>
